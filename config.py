c86833388660324976a771459d41394cd174f224
o7aso1 <qasimamin20000@gmail.com>
2026-05-18 00:46:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
-# سيحاول الكود قراءة القيم من متغيرات الاستضافة أولاً، وإذا لم يجدها سيأخذ القيم الافتراضية المكتوبة هنا
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6547204382:AAGF11w-KmizISyGLA2OYW-geZ8DfarFivE")
-
-# الـ API_ID يجب أن يكون رقماً (int)
+# 1. بيانات التليجرام والـ API (تم استخراجها من لقطات الشاشة)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7905187747:AAEvpRE2U8C94p9N3S6VqIic9rK7uO_jRDo")
 API_ID = int(os.getenv("API_ID", 24391216))
-
 API_HASH = os.getenv("API_HASH", "b40d413dc508dbbe838f5f67a68393fa")
+
+# 2. إعدادات الجلسة والتحكم (تم استخراجها من لقطات الشاشة)
+SESSION_NAME = os.getenv("SESSION_NAME", "hadar_bot")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 6432029959))
+CONTROL_CHAT = int(os.getenv("CONTROL_CHAT", -1002492160176))
