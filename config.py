8ce3541ae791c909dd2aa6e09ca47db275af7de6
o7aso1 <qasimamin20000@gmail.com>
2026-05-19 17:01:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-# 2. توكن البوت الخاص بك من BotFather
+# 1. توكن البوت الخاص بك من BotFather
 BOT_TOKEN = "6547204382:AAEmjNE1BTNLL_RTkserbU9jpAcrQVRZJUg"
 
-# 3. اسم الجلسة
+# 2. اسم الجلسة
 SESSION_NAME = "hadar_new_session"
 
-# 4. الـ Telegram User ID الخاص بحسابك لإدارة البوت
+# 3. الـ Telegram User ID الخاص بحسابك لإدارة البوت
 ADMIN_ID = 6636861893
 
-# 5. آيدي قروب التحكم (Control Chat)
+# 4. آيدي قروب التحكم (Control Chat)
 CONTROL_CHAT = 6636861893
 API_HASH = "1fbd392907063fe108230fa22b4b75c0"
 API_ID = 26082204
 RAILWAY_PUBLIC_DOMAIN = "worker-production-5580.up.railway.app"
 SUPABASE_URL = "https://fvduhxxznyezxmksnvzy.supabase.co/rest/v1/"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2ZHVoeHh6bnllenhta3Nudnp5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTE3OTM4MSwiZXhwIjoyMDk0NzU1MzgxfQ.LfbKTDfCcN1B0KgGYVnt60TWwjS3eo3jNkmv3q02buE"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2ZHVoeHh6bnplenhta3Nudnp5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTE3OTM4MSwiZXhwIjoyMDk0NzU1MzgxfQ.LfbKTDfCcN1B0KgGYVnt60TWwjS3eo3jNkmv3q02buE"
