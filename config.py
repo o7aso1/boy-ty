8447336fd2441749a28989e350d61cc4b67af1dd
o7aso1 <qasimamin20000@gmail.com>
2026-05-19 16:13:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,5 @@ CONTROL_CHAT = 6636861893
 API_HASH = "1fbd392907063fe108230fa22b4b75c0"
 API_ID = 26082204
 RAILWAY_PUBLIC_DOMAIN = "worker-production-5580.up.railway.app"
+SUPABASE_URL = "https://fvduhxxznyezxmksnvzy.supabase.co/rest/v1/"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2ZHVoeHh6bnllenhta3Nudnp5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTE3OTM4MSwiZXhwIjoyMDk0NzU1MzgxfQ.LfbKTDfCcN1B0KgGYVnt60TWwjS3eo3jNkmv3q02buE"
